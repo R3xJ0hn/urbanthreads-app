0e48e612337053a4581cabb3101c94f9924f638b
R3xJ0hn <pogirawsirex@gmail.com>
2026-02-14 13:00:30 +0800

    Modify header navbar and add shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,5 @@
-import Image from "next/image";
+import { ComponentExample } from "@/components/component-example";
 
-export default function Home() {
-  return (
-    <div className="container mx-auto px-4 py-8 h-145 flex flex-col items-center justify-center">
-      <h1 className="text-4xl font-bold mb-4">Welcome to UrbanThreads</h1>
-      <p className="text-lg mb-6 text-center">
-        Discover the latest trends in urban fashion with our exclusive collection of streetwear. From stylish hoodies to trendy sneakers, we have everything you need to elevate your wardrobe.
-      </p>
-    </div>
-  );
+export default function Page() {
+return <ComponentExample />;
 }
